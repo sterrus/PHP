334230a1f0c4aae8378a2aab2c45e8836360db54
ruslan <rgsrv@mail.ru>
2019-03-20 23:41:21 +0400

    admin_panel_delete_and_edit

--- models/articles.php
+++ models/articles.php
@@ -39,15 +39,78 @@ function articles_get($link, $id_article)
     return $article;
 }
 
-function articles_new($title, $date, $content) {
-
+function articles_new($link, $title, $date, $content) {
+    
+    //Подготовка
+    $title = trim($title);
+    $content = trim($content);
+    
+    //Проверка 
+    if ($title == '')
+        return false;
+    
+    //Запрос
+    
+    $t = "INSERT INTO articles (title, date, content) VALUES ('%s', '%s', '%s')";
+    
+    $query = sprintf($t, mysqli_real_escape_string($link, $title),
+                    mysqli_real_escape_string($link, $date),
+                    mysqli_real_escape_string($link, $content));
+    
+    $result = mysqli_query($link, $query);
+    
+    if (!$result)
+        die(mysqli_error($link));
+    
+        return true;
 }
 
-function articles_edit($id, $title, $date, $content) {
-
+function articles_edit($link, $id, $title, $date, $content) {
+  //Подготовка
+    $title = trim($title);
+    $content = trim($content);
+    $date = trim($date);
+    $id = (int)$id;
+    
+    //Проверка
+    if ($title == '')
+        return false;
+    
+    //Запрос 
+    $sql = "UPDATE articles SET title='%s', content='%s', date='%s', WHERE id='%d'";
+    
+    $query = sprintf($sql, mysqli_real_escape_string($link, $title),
+                           mysqli_real_escape_string($link, $content),
+                           mysqli_real_escape_string($link, $date),
+                           $id);
+    
+    $result = mysqli_querly($link, $query);
+    
+    if (!$result)
+        die(mysqli_error($link));
+    
+    return mysqli_affected_rows($link);
 }
 
-function articles_delete($id) {
+function articles_delete($link, $id) {
 
+    $id = (int)$id;
+    //Проверка
+    if ($id == 0)
+        return false;
+    
+    //Запрос
+    $query = sprintf("DELETE FROM articles WHERE id='%d'", $id);
+    $result = mysqli_query($link, $query);
+    
+    if (!$result)
+        die(mysqli_error($link));
+    
+    return mysqli_affected_rows($link);    
+}
+
+function articles_intro($text, $len = 500)
+{
+    return mb_substr($text, 0, $len);
 }
 ?>
---
+++ views/article_admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Мой первый блог</title>
+  <link rel="stylesheet" href="../style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <h1>Мой первый блог</h1>
+    <div>
+        <form method="post" action="index.php?action=add">
+            <label>
+                Название
+                <input type="text" name="title" value="<?=$article['title']?>" class="form-item" autofocus required>
+            </label>
+            <label>
+                Дата
+                <input type="date" name="date" value="<?=$article['date']?>" class="form-item" required>
+            </label>
+            <label>
+                Содержимое 
+                <textarea class="form-item" name="content" required><?=$article['content']?></textarea>
+            </label>
+            <input type="submit" value="Сохранить" class="btn">
+        </form>
+    </div>
+    <footer>
+      <p>Мой первый блог<br>Copyright &copy; 2019</p>
+    </footer>
+  </div>
+</body>
+</html>
